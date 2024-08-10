e9c544f06213dd5cec593b8ac997baee8dd8173e
Cesar Bermudez <cesar.bermudez@bam.com.gt>
2024-08-10 14:20:06 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button/button.component';
+import { InputComponent } from './components/input/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
+
+  inputs = [
+    {
+      type: "text",
+      label: "Name",
+      placeholder: "First name"
+    },
+    {
+      type: "number",
+      label: "Numero de telefono",
+      placeholder: "3*******"
+    },
+    {
+      type: "password",
+      label: "Passwowrd",
+      placeholder: "**********"
+    },
+    {
+      type: "email",
+      label: "Email",
+      placeholder: "email@gmail.com"
+    },
+    {
+      type: "button",
+      label: "Registrar",
+      placeholder: "email@gmail.com"
+    }
+  ];
+
+  isLogin = true;
+
+  logOut() {
+    this.isLogin = false;
+  }
 }
